851fce16528909b9e239018b75c31ad752f0a5e2
Fatma <alifatma13@gmail.com>
2020-03-05 00:00:51 +0530

    Comments

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,10 +1,22 @@
 fun main(args:Array<String>): Unit
 {
     println("Hello World")
+    //println(10-9)
+    //println(10/9)
+    //println(10.0/9.0)
+    //print(true)
 }
 
 /*
 public static void main(String[] args)
 {
-
+ println("Hello World")
 }*/
+
+// The void return type is Unit in case of Kotlin
+// Semi functional programming language.. need not have a class every time we write a code
+//kotlin uses JVM under the hood
+//Kotlin compiler creates a class file  which is loaded into the memory in execution time
+//.kt--> .class--> bytecode
+
+
